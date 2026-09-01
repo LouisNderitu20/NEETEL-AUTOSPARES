a5b71d2e26e46aa6fe8844e8d8d792751ae7fa40
Louis Nderitu <louisnderitu20@gmail.com>
2026-09-01 16:18:29 +0300

    Added mobile features

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,6 @@ const manrope = Manrope({
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
 };
 
 export const metadata: Metadata = {
